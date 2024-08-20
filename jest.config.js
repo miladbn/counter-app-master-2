b4d40b2494e4ad3ed25950e5e8e4ede6fb1fe767
miladbn <milad.b.n.77@gmail.com>
2024-08-20 15:27:25 +0330

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,6 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-  preset: "@shelf/jest-mongodb",
-  collectCoverageFrom: [
-    "components/*.{js,jsx,ts,tsx}",
-    "pages/*.{js,jsx,ts,tsx}",
-  ],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-  },
-  transformIgnorePatterns: [
-    "/node_modules/",
-    "^.+\\.module\\.(css|sass|scss)$",
-  ],
-  moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-  },
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
 };
+
+module.exports = config;
